0fcb6826538a8387f254e76505af14fa37378fe2
framere <francesco.mereto@rwth-aachen.de>
2025-07-07 10:20:46 +0200

    Update eignvalues_calc.jl

--- eignvalues_calc.jl
+++ eignvalues_calc.jl
@@ -21,13 +21,14 @@ names = ["HFbasis", "RNDbasis1", "RNDbasis2", "RNDbasis3"]
 function diagonalize_and_save(system::String)
     A = load_matrix(system)
     println("Diagonalizing the matrix for system: $system")
-    @time Σexact, Uexact = eigenvalues(A)
-    true_EVs = -sqrt.(abs(Σexact.values))
+    @time F = eigen(A)
+    true_EVs = -sqrt.(abs.(F.values))
     output_file="../Eigenvalues_folder/eigen_results_$system.jld2"
     println("Saving results to $output_file")
     jldsave(output_file; true_EVs)  # JLD2 format
     println("Done!")
 end
+
 
 for system in names
     diagonalize_and_save(system)

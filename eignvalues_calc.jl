1dbeb4ea3ebd0d2a5476860a7f83dea694ed7579
framere <francesco.mereto@rwth-aachen.de>
2025-07-08 15:16:24 +0200

    Update eignvalues_calc.jl

--- eignvalues_calc.jl
+++ eignvalues_calc.jl
@@ -30,7 +30,23 @@ function diagonalize_and_save(system::String)
 end
 
 
+# for system in names
+#     diagonalize_and_save(system)
+# end
+
+# open the file and read the eigenvalues
+function read_eigenvalues(system::String)
+    output_file = "Eigenvalues_folder/eigen_results_$system.jld2"
+    println("Reading eigenvalues from $output_file")
+    data = jldopen(output_file, "r")
+    eigenvalues = data["F.values"]
+    close(data)
+    return eigenvalues
+end
+
+# Example usage
 for system in names
-    diagonalize_and_save(system)
+    eigenvalues = read_eigenvalues(system)
+    println("Eigenvalues for system $system: ", eigenvalues)
 end
 

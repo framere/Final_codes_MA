892539043b2af67a2840f125b0449e288ad16833
framere <francesco.mereto@rwth-aachen.de>
2025-07-09 09:27:44 +0200

    Create check_diagonal_dominance.jl

---
+++ check_diagonal_dominance.jl
@@ -0,0 +1,51 @@
+using LinearAlgebra
+using Printf
+using JLD2
+
+function load_matrix(filename::String)
+    N = 27643
+    println("read ", filename)
+    file = open(filename, "r")
+    A = Array{Float64}(undef, N * N)
+    read!(file, A)
+    close(file)
+
+    A = reshape(A, N, N)
+    # A = -A
+    return Hermitian(A)
+end
+
+function check_diagonal_dominance(A::AbstractMatrix{T}) where T<:Number
+    N = size(A, 1)
+    
+    count_non_diago_dominant_rows = 0
+    for i in 1:N
+        diag_element = abs(A[i, i])
+        off_diag_sum = sum(abs(A[i, j]) for j in 1:N if j != i)
+
+        if diag_element <= off_diag_sum
+            count_non_diago_dominant_rows += 1            
+        end
+    end
+
+    return count_non_diago_dominant_rows
+end
+
+function main(system::String)
+    filename = "../formaldehyde/gamma_VASP_" * system * ".dat"
+    println("Loading matrix from: $filename")
+    A = load_matrix(filename)
+
+    non_dominant_count = check_diagonal_dominance(A)
+    if non_dominant_count > 0
+        println("Matrix is not diagonally dominant in $non_dominant_count rows.")
+    else
+        println("Matrix is diagonally dominant in all rows.")
+    end
+end
+
+systems = ["HFbasis", "RNDbasis1"] 
+
+for system in systems
+    main(system)
+end

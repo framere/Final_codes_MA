5a1da5736bef10329f3b2700e3219c45d9863bd5
framere <francesco.mereto@rwth-aachen.de>
2025-07-10 08:58:15 +0200

    Changes

---
+++ test/Check_diagonal_dominance.jl
@@ -0,0 +1,28 @@
+using LinearAlgebra
+using JLD2
+using Printf
+
+function read_eigenresults(system::String)
+    output_file = "../Eigenvalues_folder/eigen_results_$system.jld2"
+    println("Reading eigenvalues from $system")
+    data = jldopen(output_file, "r")
+    eigenvalues = data["eigenvalues"]
+    eigenvectors = data["eigenvectors"]
+    close(data)
+    return sort(eigenvalues; rev=true), eigenvectors
+end
+
+systems = ["HFbasis", "RNDbasis1"] # , "RNDbasis2", "RNDbasis3"]
+
+function geodesic_distance(eigenvectors)
+    # Calculate the geometric distance between two sets of eigenvalues
+    l = log10(eigenvectors)
+    return norm(l, 2)
+end
+
+for system in systems
+    eigenvalues, eigenvectors = read_eigenresults(system)
+    println("Top 10 eigenvalues for system $system: ", eigenvalues[1:10])
+    println("Geodesic distance for system $system: ", geodesic_distance(eigenvectors))  # Should be 0 for the same system
+end
+

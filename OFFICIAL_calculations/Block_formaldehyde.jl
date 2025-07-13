b98b40a6488e4c50324a548587fb698ff882a131
framere <francesco.mereto@rwth-aachen.de>
2025-07-13 18:54:45 +0200

    Update Block_formaldehyde.jl

--- OFFICIAL_calculations/Block_formaldehyde.jl
+++ OFFICIAL_calculations/Block_formaldehyde.jl
@@ -153,7 +153,7 @@ function main(molecule::String, l::Integer, alpha::Integer)
     end
 
     println("Davidson")
-    @time Σ, U = davidson(A, V, Naux, 1.5e-3, 500)
+    @time Σ, U = davidson(A, V, Naux, 1.5e-3, 1000)
     idx = sortperm(Σ)
     Σ = Σ[idx]
     U = U[:, idx]

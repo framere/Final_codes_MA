9f49a15f32ec4c66c1ae370adfe9f215c10f8b7f
framere <francesco.mereto@rwth-aachen.de>
2025-08-02 15:54:25 +0200

    Updates

--- TEST_smaller_syst/Si_test.jl
+++ TEST_smaller_syst/Si_test.jl
@@ -48,11 +48,11 @@ end
 
 function occupied_orbitals(molecule::String)
     if molecule == "He"
-        return 30
+        return 1
     elseif molecule == "hBN"
-        return 30
+        return 36
     elseif molecule == "Si"
-        return 30
+        return 72
     else
         error("Unknown molecule: $molecule")
     end
@@ -309,7 +309,7 @@ end
 
 betas = [32,64] #8,16,32,64, 8,16
 molecules = ["He", "hBN", "Si"]
-ls = [1, 3, 5, 10] #10, 50, 100, 200
+ls = [5, 10, 20, 50, 70] #10, 50, 100, 200
 for molecule in molecules
     println("Processing molecule: $molecule")
     for beta in betas
@@ -317,7 +317,7 @@ for molecule in molecules
         for (i, l) in enumerate(ls)
 	    nev = l*occupied_orbitals(molecule)
             println("Running with l = $nev")
-            main(molecule, nev, beta, i, 500)
+            main(molecule, nev, beta, i, 1000)
         end
     end
     println("Finished processing molecule: $molecule")

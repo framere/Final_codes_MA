f605f1a0d811b68418c8ccb390260c4e6c21635f
framere <francesco.mereto@rwth-aachen.de>
2025-08-06 17:04:16 +0200

    test

---
+++ FURTHER_TESTS/test_thresh.jl
@@ -0,0 +1,103 @@
+using LinearAlgebra
+using JLD2
+using Printf
+
+# === Global FLOP counter and helpers ===
+global NFLOPs = 0
+
+include("../FLOP_count.jl")
+
+function select_corrections_ORTHO(t_candidates, V, V_lock, η, droptol; maxorth=2)
+    ν = size(t_candidates, 2)
+    n_b = 0
+    T_hat = Matrix{eltype(t_candidates)}(undef, size(t_candidates, 1), ν)
+
+    for i in 1:ν
+        t_i = t_candidates[:, i]
+        old_norm = norm(t_i)
+        count_norm_flops(length(t_i))
+        k = 0
+
+        while k < maxorth
+            k += 1
+
+            # Count orthogonalization against V
+            count_orthogonalization_flops(1, size(V,2), size(V,1))
+            for j in 1:size(V, 2)
+                t_i -= V[:, j] * (V[:, j]' * t_i)
+            end
+
+            new_norm = norm(t_i)
+            count_norm_flops(length(t_i))
+            if new_norm > η * old_norm
+                break
+            end
+            old_norm = new_norm
+        end
+
+        if norm(t_i) > droptol
+            count_norm_flops(length(t_i))
+            n_b += 1
+            T_hat[:, n_b] = t_i / norm(t_i)
+            count_vec_scaling_flops(length(t_i))
+        end
+    end
+
+    return T_hat[:, 1:n_b], n_b
+end
+
+function occupied_orbitals(molecule::String)
+    if molecule == "H2"
+        return 1
+    elseif molecule == "formaldehyde"
+        return 6
+    else
+        error("Unknown molecule: $molecule")
+    end
+end
+
+function load_matrix(filename::String, molecule::String)
+    if molecule == "H2"
+        N = 11994
+    elseif molecule == "formaldehyde"
+        N = 27643
+    else
+        error("Unknown molecule: $molecule")
+    end
+    # println("read ", filename)
+    file = open(filename, "r")
+    A = Array{Float64}(undef, N * N)
+    read!(file, A)
+    close(file)
+
+    A = reshape(A, N, N)
+    return Hermitian(A)
+end
+
+function read_eigenresults(molecule::String)
+    output_file = "../Eigenvalues_folder/eigenres_" * molecule * "_RNDbasis1.jld2"
+    println("Reading eigenvalues from $output_file")
+    data = jldopen(output_file, "r")
+    eigenvalues = data["eigenvalues"]
+    eigenvectors = data["eigenvectors"]
+    close(data)
+    return (eigenvalues, eigenvectors)
+end
+
+molecules = ["H2", "formaldehyde"]
+
+Eigenvalues, Eigenvectors = read_eigenresults("H2")
+
+root_ev = sqrt.(abs.(Eigenvalues))
+
+diagonal_M = diagm(root_ev)
+
+root_matrix = Eigenvectors * diagonal_M * Eigenvectors'
+
+#write the matrix to a file
+output_file = "H2/root_matrix.dat"
+file = open(output_file, "w")
+write(file, root_matrix)
+close(file)
+
+

08f4f4053d45e526d804e22e62728e10102121e4
framere <francesco.mereto@rwth-aachen.de>
2025-11-06 21:50:44 +0100

    Create diagonalization.jl

---
+++ Sparsity_test/diagonalization.jl
@@ -0,0 +1,38 @@
+using LinearAlgebra
+using JLD2
+
+function load_matrix(filename::String)
+    N = 30000  
+
+    println("read ", filename)
+    file = open(filename, "r")
+    A = Array{Float64}(undef, N * N)
+    read!(file, A)
+    close(file)
+
+    A = reshape(A, N, N)
+    # A = -A
+    return Hermitian(A)
+end
+
+function diagonalize_and_save(filename::String)    
+    A = load_matrix(filename)
+    println("Diagonalizing the matrix ...")
+    @time F = eigen(A)  # F.values, F.vectors
+
+    output_file = "test_EW_results_1.jld2"
+    println("Saving results to $output_file")
+
+    jldsave(output_file; 
+        eigenvalues = F.values, 
+        eigenvectors = F.vectors
+    )
+
+    println("Done saving eigenvalues and eigenvectors.")
+end
+
+numbers = collect(1:5)
+for num in numbers
+    filename = "large_sparse_matrix_$(num).dat"
+    diagonalize_and_save(filename)
+end

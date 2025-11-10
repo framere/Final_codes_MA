b68ecc731a10c5497f030660c4b9ae9d4d2b7962
framere <francesco.mereto@rwth-aachen.de>
2025-11-10 10:12:46 +0100

    Update read_EV.jl

--- Simulate_CWNO/read_EV.jl
+++ Simulate_CWNO/read_EV.jl
@@ -9,8 +9,17 @@ function read_eigenresults(number::Integer)
     return sort(eigenvalues)
 end
 
-numbers = 1
+function read_eigenresults_mol(molecule::String)
+    output_file = "../Eigenvalues_folder/eigenres_" * molecule * "_RNDbasis1.jld2"
+    println("Reading eigenvalues from $output_file")
+    data = jldopen(output_file, "r")
+    eigenvalues = data["eigenvalues"]
+    close(data)
+    return sort(eigenvalues)
+end
 
+numbers = 1
+molecules = ["formaldehyde", "H2", "uracil"]
 for num in numbers
     Σexact = read_eigenresults(num)
     Σexact = abs.(Σexact)
@@ -21,3 +30,12 @@ for num in numbers
     println("Last 5 eigenvalues: ", Σexact[end-4:end])
 end
 
+for molecules in molecules
+    Σexact = read_eigenresults_mol(molecules)
+    Σexact = abs.(Σexact)
+    idx_exact = sortperm(Σexact, rev=true)
+    Σexact = Σexact[idx_exact]
+    println("Printing first and last 5 eigenvalues for molecule $(molecules):")
+    println("First 5 eigenvalues: ", Σexact[1:5])
+    println("Last 5 eigenvalues: ", Σexact[end-4:end])
+end

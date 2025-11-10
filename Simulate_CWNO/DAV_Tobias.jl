d78a2dda9104315bbb4af8cb771db5e7490c15f1
framere <francesco.mereto@rwth-aachen.de>
2025-11-10 11:27:12 +0100

    Update DAV_Tobias.jl

--- Simulate_CWNO/DAV_Tobias.jl
+++ Simulate_CWNO/DAV_Tobias.jl
@@ -116,7 +116,7 @@ function davidson(
     end
 end
 
-function main(number::Integer, l::Integer, alpha::Integer, min_number_iter::Integer = 5)
+function main(number::Integer, l::Integer, alpha::Integer, min_number_iter::Integer = 10)
     global NFLOPs
     NFLOPs = 0  # reset for each run
 
